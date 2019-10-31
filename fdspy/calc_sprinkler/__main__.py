5b38441a1054bd05122c7c43cdb9b4d53ee3c71c
fuyans <fuyans@gmail.com>
2019-10-31 08:00:19 +0000

    Update __main__.py

--- fdspy/calc_sprinkler/__main__.py
+++ fdspy/calc_sprinkler/__main__.py
@@ -1,7 +1,6 @@
 from fdspy.cibse_guide_e import _dT_d_dt, _Q, _theta_c, _U
 import seaborn as sns
-
-sns.set_style("ticks", {"axes.grid": True})
+sns.set_style("ticks", {'axes.grid': True, })
 
 
 if __name__ == "__main__":
@@ -10,20 +9,20 @@ if __name__ == "__main__":
     # INPUTS
     time_start = 0
     time_step = 0.1
-    time_end = 5 * 60
+    time_end = 10 * 60
 
     alpha = 0.0117e3  # [W/s2]
-    r = 2.75  # Estimation
+    r = 2.8  # Estimation
     h = 2.6
-    RTI = 115
-    T_d_activation = 273.15 + 68  # [K]
+    RTI = 130
+    T_d_activation = 273.15 + 57  # [K]
 
-    time_end = float(input("Time end [min]: ")) * 60
-    alpha = float(input("Alpha (growth factor) [W/s2]: "))
-    r = float(input("Lateral distance between fire and sprinkler heads [m]: "))
-    h = float(input("Vertical distance between sprinkler head and floor [m]: "))
-    RTI = float(input("Sprinkler RTI: "))
-    T_d_activation = float(input("Sprinkler activation temperature [C]: ")) + 273.15
+    # time_end = float(input("Time end [min]: ")) * 60
+    # alpha = float(input("Alpha (growth factor) [W/s2]: "))
+    # r = float(input("Lateral distance between fire and sprinkler heads [m]: "))
+    # h = float(input("Vertical distance between sprinkler head and floor [m]: "))
+    # RTI = float(input("Sprinkler RTI: "))
+    # T_d_activation = float(input("Sprinkler activation temperature [C]: ")) + 273.15
 
     # CONTAINERS
     time = np.arange(time_start, time_end, time_step)
@@ -48,7 +47,7 @@ if __name__ == "__main__":
     iter_time = enumerate(time)
     next(iter_time)
     for i, t in iter_time:
-        T_d[i] = T_d[i - 1] + _dT_d_dt(U[i], T_g[i], T_d[i - 1], RTI)
+        T_d[i] = T_d[i-1] + _dT_d_dt(U[i], T_g[i], T_d[i-1], RTI)
 
     # RE-EVALUATE FIRE FOR SUPPRESSION ACTIVATION
     Q[T_d >= T_d_activation] = -1
@@ -58,25 +57,37 @@ if __name__ == "__main__":
     T_d[T_d == -1] = np.max(T_d)
     T_g[T_g == -1] = np.max(T_g)
 
-    print(
-        "{:25}: {:7.2f} [min]".format(
-            "Sprinkler activated at", np.min(time[T_d == np.max(T_d)]) / 60.0
-        )
-    )
-    print("{:25}: {:7.2f} [kW]".format("The maximum HRR is", np.max(Q) / 1.0e3))
+    print("{:25}: {:7.2f} [min]".format(
+        "Sprinkler activated at", np.min(time[T_d == np.max(T_d)])/60.))
+    print("{:25}: {:7.2f} [kW]".format("The maximum HRR is", np.max(Q)/1.e3))
 
     import matplotlib.pyplot as plt
+    import seaborn as sns
+    sns.set_style('white', {'axes.grid': True,
+                            'grid.linestyle': ':', 'grid.color': '0.5', })
+    sns.set_context('paper')
+    fig, (ax0, ax1) = plt.subplots(2, 1, figsize=(3.5, 3.5), sharex='all')
+    ax1.plot(time / 60, T_g - 273.15,
+             label="Temperature (near field)", ls='-', c='k')
+    ax1.plot(time / 60, T_d - 273.15,
+             label="Temperature (sprinkler)", ls='--', c='k')
+    ax0.plot(time / 60, Q / 1e6, ls='-', c='k', label='HRR')
+    # plt.figure(num=1)
+    # plt.subplot(211)
+    # plt.plot(time/60, Q/1e6, label='HRR')
+    # plt.ylabel("Heat Release Rate [MW]")
+    # plt.subplot(212)
+    # plt.plot(time/60, T_g-273.15, label="Temperature (near field)")
+    # plt.plot(time/60, T_d-273.15, label="Temperature (sprinkler)")
+    ax1.set_xlabel("Time [min]")
+    ax1.set_ylabel("Temperature [$\degree C$]")
+    ax1.set_xticks(np.arange(0, 11, 1))
+    ax1.legend().set_visible(True)
+    ax0.set_ylabel("Heat Release Rate [MW]")
+    ax0.legend().set_visible(True)
 
-    plt.figure(num=1)
-    plt.subplot(211)
-    plt.plot(time / 60, Q / 1e6, label="HRR")
-    plt.ylabel("Heat Release Rate [MW]")
-    plt.subplot(212)
-    plt.plot(time / 60, T_g - 273.15, label="Temperature (near field)")
-    plt.plot(time / 60, T_d - 273.15, label="Temperature (sprinkler)")
-    plt.xlabel("Time [min]")
-    plt.ylabel("Temperature [$\degree C$]")
-    plt.legend()
+    plt.tight_layout()
+    # ax1.legend().set_
     plt.show()
 
     print("Press any key to continue.")
