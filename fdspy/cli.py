6330e2351fd19e0182369c0379487e14fd52ca6a
fuyans <fuyans@gmail.com>
2020-01-16 21:22:33 +0000

    Added user defined FDS source shell file

--- fdspy/cli.py
+++ fdspy/cli.py
@@ -2,18 +2,21 @@
 Usage:
     fdspy stats
     fdspy stats <file_name>
-    fdspy sbatch [-o=<int>] [-p=<int>]
+    fdspy sbatch [-o=<int>] [-p=<int>] [-v=<int_str>]
 
 Examples:
     fdspy stats
     fdspy stats residential_corridor.fds
     fdspy sbatch
-    fdspy sbatch -o 2 -p 10
+    fdspy sbatch -o 2 -p 10 -v 671
 
 Options:
     -h --help       Help.
-    -o=<int>        Optional. To specify number of OMP. 1 if undefined.
-    -p=<int>        Optional. To specify number of MPI. This will be work out based on `.fds` script if undefined.
+    -o=<int>        Optional, 1 by default. To specify number of OMP.
+    -p=<int>        Optional, -1 by default. To specify number of MPI, -1 to be worked out based on `.fds` script.
+    -v=<int_str>    Optional, 671 by default. To specify FDS source version or full FDS shell script file path. e.g. can
+                    be 671 (version number) or file path /home/installs/FDS671/bin/FDS6VARS.sh.
+
 
 Commands:
     fdspy stats     To analysis all .fds files in the current working directory.
@@ -22,16 +25,20 @@ Commands:
 """
 
 import os
+import typing
 import plotly
 import subprocess
 from docopt import docopt
 from fdspy.lib.fds_script_proc_analyser import main_cli
 
 
+filepath_fds_source_template = '/home/installs/FDS{}/bin/FDS6VARS.sh'
+
+
 def stats_single_fds_file(filepath_fds: str):
 
     dict_out = main_cli(filepath_fds=filepath_fds)
-    print(dict_out["str"])
+
     with open(filepath_fds + ".stats.txt", "w+") as f:
         f.write(dict_out["str"])
 
@@ -48,12 +55,23 @@ def stats_single_fds_file(filepath_fds: str):
         },
     )
 
+    return dict_out
 
-def sbatch_single_fds_file(filepath_fds: str, n_omp: int = 1, n_mpi: int = None):
+
+def sbatch_single_fds_file(filepath_fds: str, n_omp: int = 1, n_mpi: int = -1, fds_v: str = 671):
+
+    # fetch fds source shell script
+    try:
+        filepath_fds_source = filepath_fds_source_template.format(f'{int(fds_v)}')
+    except ValueError:
+        filepath_fds_source = fds_v
+
+    # if os.path.isfile(os.path.realpath(filepath_fds_source)):
+    #     raise ValueError('FDS source shell script not defined.')
 
     # make sh file
     from fdspy.lib.sbatch import make_sh
-    sh = make_sh(filepath_fds=filepath_fds, n_omp=n_omp, n_mpi=n_mpi)
+    sh = make_sh(filepath_fds=filepath_fds, filepath_fds_source=filepath_fds_source, n_omp=n_omp, n_mpi=n_mpi)
 
     # write sh file
     filename_sh = 'submit.sh'
@@ -101,9 +119,13 @@ def main():
         for i in fn:
             if i.endswith(".fds"):
                 stats_single_fds_file(i)
+                print('FDS script statistics is successfully generated.')
+
                 sbatch_single_fds_file(
                     filepath_fds=i,
                     n_omp=arguments["-o"] or 1,
-                    n_mpi=arguments["-p"] or None
+                    n_mpi=arguments["-p"] or -1,
+                    fds_v=arguments["-v"] or 671
                 )
+                print('Shell file is successfully saved and executed.')
                 break
--- fdspy/lib/sbatch.py
+++ fdspy/lib/sbatch.py
@@ -9,13 +9,27 @@ import pandas as pd
 import plotly.express as pex
 from fdspy.lib.fds_script_proc_decoder import *
 
-sh_template = '''#!/bin/sh\n\n#SBATCH -J {name}\n#SBATCH -n {n_mpi}\n#SBATCH -p compute\n#SBATCH -e error-%j.err\n#SBATCH -o output-%j.out\n\nsource {fds_source}\nexport OMP_NUM_THREADS={n_omp}\nmpiexec -bootstrap slurm -np $SLURM_NTASKS fds {filename_fds}\n'''
+# sh_template = '''#!/bin/sh\n\n#SBATCH -J {name}\n#SBATCH -n {n_mpi}\n#SBATCH -p compute\n#SBATCH -e error-%j.err\n#SBATCH -o output-%j.out\n\nsource {fds_source}\nexport OMP_NUM_THREADS={n_omp}\nmpiexec -bootstrap slurm -np $SLURM_NTASKS fds {filename_fds}\n'''
+sh_template = '\n'.join([
+    '#!/bin/sh',
+    '',
+    '#SBATCH -J {name}',
+    '#SBATCH -n {n_mpi}',
+    '#SBATCH -p compute',
+    '#SBATCH -e error-%j.err',
+    '#SBATCH -o output-%j.out',
+    '',
+    'source {fds_source}',
+    'export OMP_NUM_THREADS={n_omp}',
+    '',
+    'mpiexec -bootstrap slurm -np $SLURM_NTASKS fds {filename_fds}',
+])
 
 
 def make_sh(
         filepath_fds: str,
-        fds_source: str = r'/home/installs/FDS671/bin/FDS6VARS.sh',
-        n_mpi: int = None,
+        filepath_fds_source: str,
+        n_mpi: int = -1,
         n_omp: int = 1,
 ) -> str:
 
@@ -35,7 +49,8 @@ def make_sh(
     job_name = os.path.basename(filepath_fds).replace('.fds', '')
 
     # work out number of mpi
-    if n_mpi is None:
+    print(n_mpi, type(n_mpi))
+    if n_mpi < 1:
         try:
             n_mpi = len(set(df['MPI_PROCESS'].dropna().values))
         except KeyError:
@@ -43,7 +58,7 @@ def make_sh(
         n_mpi = 1 if n_mpi < 1 else n_mpi
 
     # make sh file
-    sh = sh_template.format(name=job_name, n_mpi=n_mpi, fds_source=fds_source, n_omp=n_omp, filename_fds=filename_fds)
+    sh = sh_template.format(name=job_name, n_mpi=n_mpi, fds_source=filepath_fds_source, n_omp=n_omp, filename_fds=filename_fds)
 
     return sh
 
