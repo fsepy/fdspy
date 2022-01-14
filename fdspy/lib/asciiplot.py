85a7bf453fc19915c111aa73205c539c2ad9ec3a
fuyans <fuyans@gmail.com>
2022-01-14 15:12:29 +0000

    Test optimisation.

--- fdspy/lib/asciiplot.py
+++ fdspy/lib/asciiplot.py
@@ -251,23 +251,3 @@ class AsciiPlot:
             raise ValueError('`ylim` should be a tuple with length equal to 2')
 
         self.__ylim = v
-
-
-def _test_asciiplot():
-    size = (80, 25)
-    xlim = (-2*np.pi, 2*np.pi)
-    ylim = (-1.1, 1.1)
-
-    aplot = AsciiPlot(size=size)
-
-    x = np.linspace(-2*np.pi, 2*np.pi, 50)
-    aplot.plot(
-        x=x,
-        y=np.sin(x),
-        xlim=xlim,
-        ylim=ylim,
-    ).show()
-
-
-if __name__ == '__main__':
-    _test_asciiplot()
---
+++ fdspy/lib/asciiplot_test.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from .asciiplot import *
+
+
+def test_asciiplot():
+    size = (80, 25)
+    xlim = (-2 * np.pi, 2 * np.pi)
+    ylim = (-1.1, 1.1)
+
+    aplot = AsciiPlot(size=size)
+
+    x = np.linspace(-2 * np.pi, 2 * np.pi, 50)
+    aplot.plot(
+        x=x,
+        y=np.sin(x),
+        xlim=xlim,
+        ylim=ylim,
+    ).show()
