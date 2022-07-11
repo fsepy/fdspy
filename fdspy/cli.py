fa91805ba8505ad79c1a10495dbf56fadaa6201d
fuyans <fuyans@gmail.com>
2022-07-11 12:11:48 +0100

    Fixed sbatch sub_parser

--- fdspy/cli.py
+++ fdspy/cli.py
@@ -72,52 +72,60 @@ def helper_get_list_filepath_end_width(cwd: str, end_with: str) -> list:
 def main():
     import argparse
 
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description='FDS Python Helper Tools')
+    subparsers = parser.add_subparsers(dest='sbatch')
 
-    parser.add_argument('sbatch',
-                        help='To perform `fdspy stats`, generate a `sbatch` shell script file and run the shell script file with `sbash`.',
-                        action='store_true')
-    parser.add_argument('-o', '--omp',
-                        help='Number of OMP. Default 1.',
-                        nargs='?',
-                        default=1,
-                        type=int)
-    parser.add_argument('-p', '--mpi',
-                        help='Number of MPI. If undefined, -p will be set based on the *.fds file by the following:'
-                             '1. If `MPI_PROCESS` is defined for each and every `MESH`: -p will be equal to the greatest MPI_PROCESS.'
-                             '2. If `MPI_PROCESS` is defined for some but not all `MESH`: -p will be set to 1.'
-                             '3. If `MPI_PROCESS` is not defined for all `MESH`: -p will be set to the number of `MESH`',
-                        nargs='?',
-                        default=-1,
-                        type=int)
-    parser.add_argument('-v', '--fds-version',
-                        help=f'FDS source version or full FDS shell script file path. e.g. can be 671 (version number) ',
-                        nargs='?',
-                        default='671',
-                        type=str)
-    parser.add_argument('--mail-type',
-                        help='Notify user by email when certain event types occur. Valid type values are NONE, BEGIN, END, FAIL, REQUEUE, ALL (equivalent to BEGIN, END, FAIL, REQUEUE, and STAGE_OUT), STAGE_OUT (burst buffer stage out and teardown completed), TIME_LIMIT, TIME_LIMIT_90 (reached 90 percent of time limit), TIME_LIMIT_80 (reached 80 percent of time limit), TIME_LIMIT_50 (reached 50 percent of time limit) and ARRAY_TASKS (send emails for each array task). Multiple type values may be specified in a comma separated list. The user to be notified is indicated with --mail-user. Unless the ARRAY_TASKS option is specified, mail notifications on job BEGIN, END and FAIL apply to a job array as a whole rather than generating individual email messages for each task in the job array.',
-                        nargs='?',
-                        default='NONE',
-                        type=str)
-    parser.add_argument('--mail-user',
-                        help=f'User to receive email notification of state changes as defined by --mail-type. The default value is the submitting user.',
-                        nargs='?',
-                        default='NONE',
-                        type=str)
-    parser.add_argument('filepath',
-                        help=f'Input file name (including extension), use first *.fds file found in the current working directory if not provided.',
-                        nargs='?',
-                        default=None,
-                        type=str)
+    p_sbatch = subparsers.add_parser('sbatch',
+        help='To perform `fdspy stats`, generate a `sbatch` shell script file and run the shell script file with '
+             '`sbash`.'
+    )
+
+    p_sbatch.add_argument('-o', '--omp',
+                          help='Number of OMP. Default 1.',
+                          nargs='?',
+                          default=1,
+                          type=int)
+    p_sbatch.add_argument('-p', '--mpi',
+                          help='Number of MPI. If undefined, -p will be set based on the *.fds file by the following:'
+                               '1. If `MPI_PROCESS` is defined for each and every `MESH`: -p will be equal to the greatest MPI_PROCESS.'
+                               '2. If `MPI_PROCESS` is defined for some but not all `MESH`: -p will be set to 1.'
+                               '3. If `MPI_PROCESS` is not defined for all `MESH`: -p will be set to the number of `MESH`',
+                          nargs='?',
+                          default=-1,
+                          type=int)
+    p_sbatch.add_argument('-v', '--fds-version',
+                          help=f'FDS source version or full FDS shell script file path. e.g. can be 671 (version number) ',
+                          nargs='?',
+                          default='671',
+                          type=str)
+    p_sbatch.add_argument('--mail-type',
+                          help='Notify user by email when certain event types occur. Valid type values are NONE, BEGIN, END, FAIL, REQUEUE, ALL (equivalent to BEGIN, END, FAIL, REQUEUE, and STAGE_OUT), STAGE_OUT (burst buffer stage out and teardown completed), TIME_LIMIT, TIME_LIMIT_90 (reached 90 percent of time limit), TIME_LIMIT_80 (reached 80 percent of time limit), TIME_LIMIT_50 (reached 50 percent of time limit) and ARRAY_TASKS (send emails for each array task). Multiple type values may be specified in a comma separated list. The user to be notified is indicated with --mail-user. Unless the ARRAY_TASKS option is specified, mail notifications on job BEGIN, END and FAIL apply to a job array as a whole rather than generating individual email messages for each task in the job array.',
+                          nargs='?',
+                          default='NONE',
+                          type=str)
+    p_sbatch.add_argument('--mail-user',
+                          help=f'User to receive email notification of state changes as defined by --mail-type. The default value is the submitting user.',
+                          nargs='?',
+                          default='NONE',
+                          type=str)
+    p_sbatch.add_argument('filepath',
+                          help=f'Input file name (including extension), use first *.fds file found in the current working directory if not provided.',
+                          nargs='?',
+                          default=None,
+                          type=str)
 
     args = parser.parse_args()
+    import pprint
+    pprint.pprint(args)
 
     # get file path
     if args.filepath is not None:
         args.filepath = os.path.realpath(args.filepath)
     else:
-        args.filepath = helper_get_list_filepath_end_width(os.getcwd(), '.fds')[0]
+        try:
+            args.filepath = helper_get_list_filepath_end_width(os.getcwd(), '.fds')[0]
+        except IndexError:
+            raise IndexError(f'Unable to find any *.fds file in directory {os.getcwd()}')
 
     if args.sbatch:
         sbatch(**args.__dict__)
